832e36884d3fbe950cd00bc30d3233c4d2830341
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2024-05-07 17:28:06 +0200

    Terminamos el script de la creacion de las tablas

--- Tables.sql
+++ Tables.sql
@@ -36,3 +36,53 @@ create table clients
     client_password varchar2(18) not null
 )
 
+create table orders
+(
+    order_id varchar2(4) primary key,
+    order_date date,
+    direction varchar2(100),
+    client_id varchar2(4),
+    foreign key (client_id) references clients(client_id)
+);
+
+create table allergens
+(
+    allergen_id varchar2(4) primary key,
+    allergen_name varchar2(20)
+);
+
+create table categories
+(
+    categoriy_id varchar2(4) primary key,
+    categoriy_name varchar2(20)
+);
+
+create table products
+(
+    product_id varchar2(4) primary key,
+    product_name varchar2(50),
+    product_description varchar2(300),
+    price number(5,2),
+    categoriy_id varchar2(4),
+    foreign key (categoriy_id) references categories(categoriy_id)
+);
+
+create table allergensproduct
+(
+    product_id varchar2(4) primary key,
+    allergen_id varchar2(4) primary key
+);
+
+create table orderdetails
+(
+    orderdetail_id varchar2(4) primary key,
+    product_name varchar2(50),
+    quantity_product number(2),
+    sales_price number(5,2),
+    product_id varchar2(4),
+    foreign key (product_id) references products(product_id),
+    employee_id varchar2(4),
+    foreign key (employee_id) references employees(employee_id)
+    order_id varchar2(4),
+    foreign key (order_id) references orders(order_id)
+);
