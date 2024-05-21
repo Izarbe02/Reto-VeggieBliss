05547a1a534877aa1b2b74128fe8f3b4e8c9d752
Izarbe02 <a27302@svalero.com>
2024-05-21 13:07:29 +0200

    Js CAtegorias

--- js/categorias.js
+++ js/categorias.js
@@ -58,4 +58,90 @@ const printBurguerData = (burguerData) =>
             `
         })
     }
+
+    
+const printSaladData = (saladData) => {
+    const category = document.getElementsByClassName('categories')[0];
+    Array.from(saladData).forEach(salad => {
+        const container = document.createElement("div");
+        container.classList.add("container", "salads");
+        category.appendChild(container);
+        container.innerHTML = `
+            <div class="category-card">
+                <div class="category-header">
+                    <h2>${salad.product_name}</h2>
+                </div>
+                <img src="${salad.product_image}" alt="${salad.product_name}">
+                <div class="description">
+                    <p>${salad.product_description}</p>
+                </div>
+                <button class="orange-button">+</button>
+            </div>
+        `;
+    });
+}
+
+const printRazzionData = (razzionData) => {
+    const category = document.getElementsByClassName('categories')[0];
+    Array.from(razzionData).forEach(razzion => {
+        const container = document.createElement("div");
+        container.classList.add("container", "razzions");
+        category.appendChild(container);
+        container.innerHTML = `
+            <div class="category-card">
+                <div class="category-header">
+                    <h2>${razzion.product_name}</h2>
+                </div>
+                <img src="${razzion.product_image}" alt="${razzion.product_name}">
+                <div class="description">
+                    <p>${razzion.product_description}</p>
+                </div>
+                <button class="orange-button">+</button>
+            </div>
+        `;
+    });
+}
+
+const printDessertData = (dessertData) => {
+    const category = document.getElementsByClassName('categories')[0];
+    Array.from(dessertData).forEach(dessert => {
+        const container = document.createElement("div");
+        container.classList.add("container", "desserts");
+        category.appendChild(container);
+        container.innerHTML = `
+            <div class="category-card">
+                <div class="category-header">
+                    <h2>${dessert.product_name}</h2>
+                </div>
+                <img src="${dessert.product_image}" alt="${dessert.product_name}">
+                <div class="description">
+                    <p>${dessert.product_description}</p>
+                </div>
+                <button class="orange-button">+</button>
+            </div>
+        `;
+    });
+}
+
+
+const printDrinkData = (drinkData) => {
+    const category = document.getElementsByClassName('categories')[0];
+    Array.from(drinkData).forEach(drink => {
+        const container = document.createElement("div");
+        container.classList.add("container", "drinks");
+        category.appendChild(container);
+        container.innerHTML = `
+            <div class="category-card">
+                <div class="category-header">
+                    <h2>${drink.product_name}</h2>
+                </div>
+                <img src="${drink.product_image}" alt="${drink.product_name}">
+                <div class="description">
+                    <p>${drink.product_description}</p>
+                </div>
+                <button class="orange-button">+</button>
+            </div>
+        `;
+    });
+}
 fetchData()
